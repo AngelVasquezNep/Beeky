08a34c40674eef0206eed75f71c8bef71f41824c
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2021-02-01 17:57:56 -0600

    Add theme file

---
+++ app/theme/index.js
@@ -0,0 +1,16 @@
+/**
+ * To use as props in components
+ * 
+ * Always update global.css
+ */
+
+export const COLORS = {
+  primary: '#34cfd4',
+  lightGray: '#b1b1b1',
+  white: '#FFFFFF',
+};
+
+export const FONT_SIZES = {
+  small: '1rem',
+  tiny: '0.8rem',
+};
