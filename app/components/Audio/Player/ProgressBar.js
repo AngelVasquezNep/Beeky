815e286120e1897b59bce92493b0657b6bc34d83
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2021-02-01 17:37:11 -0600

    Improve ProgressBar Player component

--- app/components/Audio/Player/ProgressBar.js
+++ app/components/Audio/Player/ProgressBar.js
@@ -1,6 +1,11 @@
+import PropTypes from 'prop-types';
+import { formatDuration, toHHMMSS } from 'utils/date';
+
 import Range from 'components/Range';
 
 import styles from './styles.module.css';
+
+const getOffset = ({ value, max }) => `${(value / max) * 100 - value * 0.5}%`;
 
 const ProgressBar = ({ value, max, handleChange }) => {
   return (
@@ -9,9 +14,29 @@ const ProgressBar = ({ value, max, handleChange }) => {
         value={value}
         max={max}
         onChange={({ target }) => handleChange(Number(target.value))}
+        onMouseOver={({ target }) => console.log({ target })}
       />
+
+      <span
+        style={{ left: getOffset({ value, max }) }}
+        className={styles['ProgressBar-value']}
+      >
+        {toHHMMSS(value)}
+      </span>
+
+      <span
+        className={styles['ProgressBar-max']}
+      >
+        {toHHMMSS(max)}
+      </span>
     </div>
   );
+};
+
+ProgressBar.propTypes = {
+  value: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default ProgressBar;
