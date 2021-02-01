4bfa398537ff0c77d0a24c7a52e2f908cf719fbb
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2021-01-31 21:58:04 -0600

    Add logic to manage multiple audio events

--- app/components/Audio/Player/index.js
+++ app/components/Audio/Player/index.js
@@ -1,32 +1,103 @@
-import { formatDuration } from 'utils/date'
+import { toHHMMSS } from 'utils/date';
+
+import Audio from 'components/Audio';
 
 const Player = ({
+  title,
+  currentTime,
   duration,
+  loading,
   src,
   setRef,
   play,
   isPlaying,
+  playbackRate,
+  handlePlaybackRageChanges,
+  volume,
+  handleVolumeChanges,
+  muted,
+  setCurrentTime,
+  handleToggleMute,
+  handleProgress,
   onTogglePlay,
-  handleOnLoadedMetadata,
+  handleTimeUpdate,
+  handleWaiting,
+  handlePlaying,
+  handleSeeking,
+  handleSeeked,
+  handleLoadedData,
+  handleLoadedMetadata,
 }) => {
   return (
     <div>
-      <p>Duration: {formatDuration(duration)}</p>
+      {loading && <p>Cargando...</p>}
+      <p>{title}</p>
+      {duration > 0 && (
+        <p>
+          {toHHMMSS(currentTime)} / {toHHMMSS(duration)}
+        </p>
+      )}
       <button onClick={onTogglePlay}>{isPlaying ? 'Pause' : 'Play'} </button>
-      <audio
+      <div>
+        <input
+          type="range"
+          min="0.5"
+          max="2"
+          step={0.1}
+          value={playbackRate}
+          onChange={({ target }) =>
+            handlePlaybackRageChanges(Number(target.value))
+          }
+        />
+        {playbackRate}x
+      </div>
+
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="1"
+          step={0.1}
+          value={volume}
+          onChange={({ target }) => handleVolumeChanges(Number(target.value))}
+        />
+        {volume} vol{' '}
+        <button onClick={handleToggleMute}>
+          {muted ? '+ Sound' : '- Mute'}
+        </button>
+      </div>
+
+      <div>
+        <button onClick={() => setCurrentTime(parseInt(currentTime) - 10)}>
+          -10
+        </button>
+        <button onClick={() => setCurrentTime(parseInt(currentTime) + 10)}>
+          +10
+        </button>
+      </div>
+
+      <Audio
         play={play.toString()}
-        ref={setRef}
+        setRef={setRef}
         controls={false}
         src={src}
-        onLoadedMetadata={handleOnLoadedMetadata}
+        handleProgress={handleProgress}
+        handleSeeking={handleSeeking}
+        handleSeeked={handleSeeked}
+        handleLoadedData={handleLoadedData}
+        handleWaiting={handleWaiting}
+        handlePlaying={handlePlaying}
+        handleLoadedMetadata={handleLoadedMetadata}
+        handleTimeUpdate={({ target }) =>
+          handleTimeUpdate(Number(target.currentTime))
+        }
       />
     </div>
   );
 };
 
 Player.defaultProps = {
-  play: false,
-  duration: 0, // In seconds
-}
+  title: 'Title',
+};
 
 export default Player;
--- app/containers/Audio/Player/index.js
+++ app/containers/Audio/Player/index.js
@@ -4,6 +4,11 @@ import { AudioPlayer } from 'components';
 
 const INITIAL_PLAYER_STATE = {
   duration: 0,
+  currentTime: 0,
+  playbackRate: 1,
+  volume: 0.5,
+  lastVolume: 0.5,
+  muted: false,
 };
 
 const mergeAudioInfo = (newInfo) => (currentAudioInfo) => ({
@@ -11,21 +16,37 @@ const mergeAudioInfo = (newInfo) => (currentAudioInfo) => ({
   ...newInfo,
 });
 
+const isEnded = (audio) => {
+  const { duration, currentTime } = audio;
+
+  return currentTime >= duration;
+};
+
+/**
+ * calculareAudioBuffered
+ * @param {TimeRanges} - Use like: audio.buffered
+ */
+const calculareAudioBuffered = (audioBuffered) =>
+  Array.from(audioBuffered, (_, i) => ({
+    start: audioBuffered.start(i),
+    end: audioBuffered.end(i),
+  }));
+
 const PlayerContainer = ({ src }) => {
   const audioRef = useRef(null);
   const [audio, setAudio] = useState(null);
   const [audioInfo, setAudioInfo] = useState(INITIAL_PLAYER_STATE);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const audio = audioRef.current;
     setAudio(audio);
+    setLoading(false);
 
-    setAudioInfo(
-      mergeAudioInfo({
-        duration: audio.duration,
-      }),
-    );
+    updateAudioInfo({
+      duration: audio.duration || 0,
+    });
   }, [audioRef]);
 
   useEffect(() => {
@@ -33,23 +54,81 @@ const PlayerContainer = ({ src }) => {
       if (isPlaying) {
         audio.play();
       } else {
+        setLoading(false);
         audio.pause();
       }
     }
   }, [isPlaying]);
 
-  const updateDuration = ({ target }) =>
+  useEffect(() => {
+    if (audio) {
+      audio.volume = audioInfo.volume;
+      audio.currentTime = audioInfo.currentTime;
+      audio.playbackRate = audioInfo.playbackRate;
+    }
+  }, [audioInfo.volume, audioInfo.currentTime, audioInfo.playbackRate]);
+
+  const updateAudioInfo = (newInfo) => setAudioInfo(mergeAudioInfo(newInfo));
+
+  const handleLoadedMetadata = ({ target }) => {
+    setLoading(false);
     setAudioInfo(mergeAudioInfo({ duration: target.duration }));
+  };
+
+  const handleProgress = ({ target }) => {
+    // TODO create a buffer progress bar
+    const buffered = calculareAudioBuffered(target.buffered);
+    console.log({ buffered });
+  };
+
+  const handleTimeUpdate = (currentTime) => {
+    updateAudioInfo({ currentTime });
+
+    if (isEnded(audio)) {
+      setIsPlaying(false);
+      updateAudioInfo({ currentTime: 0 });
+    }
+  };
+
+  const handlePlaybackRageChanges = (playbackRate) =>
+    updateAudioInfo({ playbackRate });
+
+  const handleVolumeChanges = (volume) => {
+    const lastVolume = volume === 0 ? audioInfo.lastVolume : volume;
+
+    updateAudioInfo({ volume, lastVolume, muted: volume === 0 });
+  };
+
+  const handleToggleMute = () => {
+    const volume = audioInfo.muted ? audioInfo.lastVolume : 0;
+
+    updateAudioInfo({ volume, muted: !audioInfo.muted });
+  };
+
+  const handleSetCurrentTime = (currentTime) =>
+    updateAudioInfo({ currentTime });
 
   return (
     <AudioPlayer
       {...audioInfo}
       src={src}
+      loading={loading}
       isPlaying={isPlaying}
       play={isPlaying}
       setRef={audioRef}
-      handleOnLoadedMetadata={updateDuration}
+      handleLoadedMetadata={handleLoadedMetadata}
       onTogglePlay={() => setIsPlaying((isPlaying) => !isPlaying)}
+      handleSeeking={() => setLoading(true)}
+      handleSeeked={() => setLoading(false)}
+      handleLoadedData={() => setLoading(false)}
+      handleWaiting={() => setLoading(true)}
+      handlePlaying={() => setLoading(false)}
+      handleTimeUpdate={handleTimeUpdate}
+      handleProgress={handleProgress}
+      handlePlaybackRageChanges={handlePlaybackRageChanges}
+      handleVolumeChanges={handleVolumeChanges}
+      handleToggleMute={handleToggleMute}
+      setCurrentTime={handleSetCurrentTime}
     />
   );
 };
