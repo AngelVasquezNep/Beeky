92014c4da786faf8163acc96d7dcb806521a1e4e
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2021-01-31 21:39:17 -0600

    Add toHHMMSS date util

--- app/utils/date/index.js
+++ app/utils/date/index.js
@@ -14,11 +14,11 @@ const UNITS = [
 /**
  * formatDuration
  * @param {Number} seconds
- * @returns {String} - Like 6 second(s) 
+ * @returns {String} - Like '6 second(s)'
  */
 export function formatDuration(seconds) {
   if (seconds <= 0) {
-    return 'now';
+    return '0 second(s)';
   }
 
   const r = [];
@@ -37,3 +37,20 @@ export function formatDuration(seconds) {
 
   return r.join(', ');
 }
+
+/**
+ * toHHMMSS
+ * @param {Number} secs
+ * @returns {String} - 10:00
+ */
+export const toHHMMSS = (secs) => {
+  const sec_num = parseInt(secs, 10);
+  const hours = Math.floor(sec_num / 3600);
+  const minutes = Math.floor(sec_num / 60) % 60;
+  const seconds = sec_num % 60;
+
+  return [hours, minutes, seconds]
+    .map((v) => (v < 10 ? '0' + v : v))
+    .filter((v, i) => v !== '00' || i > 0)
+    .join(':');
+};
