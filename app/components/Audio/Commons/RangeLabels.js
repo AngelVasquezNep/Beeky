9d3b76993fad8c9d40fcc40e4c5ed5427b89d709
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2021-02-06 20:18:01 -0600

    Create Audio RangLabels components

---
+++ app/components/Audio/Commons/RangeLabels.js
@@ -0,0 +1,27 @@
+import classnames from 'classnames';
+
+import { toHHMMSS } from 'app/utils/date';
+import { getOffset } from 'components/utils';
+
+import styles from './RangeLabels.module.css';
+
+export const RangeLabelCurrentValue = ({ value, min, max, className }) => (
+  <span
+    style={{ ...getOffset({ value, max, min }) }}
+    className={classnames(styles.RangeLabelCurrentValue, className)}
+  >
+    {toHHMMSS(value)}
+  </span>
+);
+
+export const RangeLabelMaxValue = ({ value, className }) => (
+  <span className={classnames(styles.RangeLabelMaxValue, className)}>
+    {toHHMMSS(value)}
+  </span>
+);
+
+export const RangeLabelMinValue = ({ value, className }) => (
+  <span className={classnames(styles.RangeLabelMinValue, className)}>
+    {toHHMMSS(value)}
+  </span>
+);
