de5d0588aa534c6164aad9d157693300c83b6787
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2021-02-08 22:24:12 -0600

    Add Close, Edit and Trash Icons

--- app/components/Icons/index.js
+++ app/components/Icons/index.js
@@ -2,9 +2,12 @@ import ArrowLeftIcon from './ArrowLeft';
 import ArrowRightIcon from './ArrowRight';
 import AudioBookmarkIcon from './AudioBookmark';
 import BookmarkIcon from './Bookmark';
+import CloseIcon from './Close'
+import EditIcon from './Edit'
 import MuteIcon from './MuteIcon'
 import PauseIcon from './Pause';
 import PlayIcon from './Play';
+import TrashIcon from './Trash'
 import VolumeIcon from './VolumeIcon'
 
 export {
@@ -12,8 +15,11 @@ export {
   ArrowRightIcon,
   AudioBookmarkIcon,
   BookmarkIcon,
+  CloseIcon,
+  EditIcon,
   MuteIcon,
   PauseIcon,
   PlayIcon,
+  TrashIcon,
   VolumeIcon,
 };
