a8af40e78f735c982eff9b568baf78db929eaf94
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2021-02-09 21:55:07 -0600

    Use PlayerApp Layout as main component into home page

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,54 @@
-import { AudioPlayerContainer } from 'containers';
-import { AudioPlayer, AudioClip } from 'components';
+import { useState } from 'react';
+
+import uuid from 'app/utils/uuid';
+
+import PlayerApp from 'app/layouts/PlayerApp';
 
 import styles from '../styles/Home.module.css';
 
 function Home() {
+  const [clips, setClips] = useState([]);
+
+  function saveClip(clip) {
+    const createdAt = new Date().getTime();
+
+    setClips((clips) => [
+      ...clips,
+      {
+        id: uuid(),
+        ...clip,
+        createdAt,
+        updatedAt: createdAt,
+      },
+    ]);
+  }
+
+  function handleUpdateClip(clip) {
+    setClips((clips) =>
+      clips.map((c) =>
+        c.id === clip.id
+          ? { ...c, ...clip, updatedAt: new Date().getTime() }
+          : c,
+      ),
+    );
+  }
+
+  function handleDeleteClip(clipId) {
+    setClips((clips) => clips.filter((clip) => clip.id !== clipId));
+  }
+
   return (
     <div className={styles.container}>
-      <AudioPlayerContainer
+      <PlayerApp
+        clips={clips}
+        saveClip={saveClip}
+        handleUpdateClip={handleUpdateClip}
+        handleDeleteClip={handleDeleteClip}
+        bookId="123"
+        minClipDuration={30}
         title="Cien años de soledad"
-        src="http://listen.vo.llnwd.net/g3/prvw/3/2/6/0/4/2270840623.mp3"
+        src="https://samples.findawayworld.com/363461/363461_sample.mp3"
         cover="https://horcrux1.beek.io/caWKfzkoF0sNwmWPqRNBSO1lMqk=/264x394/smart/store/507ea783cdc56e18967b431ad8ad3b140a58e0456810f9c9ce8ed188354d"
-        AudioPlayerComponent={AudioPlayer}
-      />
-      <AudioPlayerContainer
-        src="http://listen.vo.llnwd.net/g3/prvw/3/2/6/0/4/2270840623.mp3"
-        AudioPlayerComponent={AudioClip}
-        initialValues={{
-          from: 10,
-          to: 21,
-          muted: true,
-        }}
       />
     </div>
   );
